f74e37e1016901e175a9e50fcf732c0b11f33b28
zhangy-by <zhangy-by@glodon.com>
2018-05-17 17:22:52 +0800

    添加图片处理方法

---
+++ app/src/main/java/com/zhangy_by/opencv/CameraActivity.java
@@ -0,0 +1,18 @@
+package com.zhangy_by.opencv;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zhangy-by on 2018/5/17.
+ */
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
--- app/src/main/java/com/zhangy_by/opencv/MainActivity.java
+++ app/src/main/java/com/zhangy_by/opencv/MainActivity.java
@@ -1,20 +1,34 @@
 package com.zhangy_by.opencv;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.nfc.Tag;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import io.reactivex.Observable;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private ImageView iv;
-    private Button btn;
 
     private String TAG = "xxxxxxxxx";
 
@@ -34,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         iv = findViewById(R.id.imageView);
-        btn = findViewById(R.id.button);
 
         if (OpenCVLoader.initDebug()) {
             Log.e(TAG, "onCreate: success");
@@ -42,18 +54,99 @@ public class MainActivity extends AppCompatActivity {
             Log.e(TAG, "onCreate: fail");
         }
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                BitmapFactory.Options options = new BitmapFactory.Options();
-                options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.cat, options);
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.cat);
+                //创建两个矩阵（容器）对象，原图像（src），输出图像（dst）
+                Mat src = new Mat();
+                Mat dst = new Mat();
+                //将一张图片变成矩阵对象
+                Utils.bitmapToMat(bitmap, src);
+                //进行图像彩色空间转换
+                Imgproc.cvtColor(src, dst, Imgproc.COLOR_BGR2GRAY);
+                //将矩阵对象变成图片
+                Utils.matToBitmap(dst, bitmap);
+                iv.setImageBitmap(bitmap);
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.cat);
                 Mat src = new Mat();
                 Mat dst = new Mat();
                 Utils.bitmapToMat(bitmap, src);
                 Imgproc.cvtColor(src, dst, Imgproc.COLOR_BGR2GRAY);
+                Mat blur1 = new Mat();
+                Mat blur2 = new Mat();
+                Imgproc.GaussianBlur(dst, blur1, new Size(5, 5), 5);
+                Imgproc.GaussianBlur(dst, blur2, new Size(7, 7), 5);
+                Mat diff = new Mat();
+                Core.absdiff(blur1, blur2, diff);
+                Core.multiply(diff, new Scalar(100), diff);
+                Imgproc.threshold(diff, diff, 50, 255, Imgproc.THRESH_BINARY_INV);
+
+                //Imgproc.Laplacian(src, dst, -1, 3, 1, 0, Core.BORDER_DEFAULT);
+                Utils.matToBitmap(diff, bitmap);
+                iv.setImageBitmap(bitmap);
+            }
+        });
+
+        findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.cat);
+                Mat src = new Mat();
+                Mat dst = new Mat();
+                Utils.bitmapToMat(bitmap, src);
+                Imgproc.blur(src, dst, new Size(100, 100));
                 Utils.matToBitmap(dst, bitmap);
                 iv.setImageBitmap(bitmap);
+            }
+        });
+
+        findViewById(R.id.button5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.girl);
+                Observable.just(bitmap).map(new Function<Bitmap, Bitmap>() {
+                    @Override
+                    public Bitmap apply(Bitmap bitmap) throws Exception {
+                        Mat src = new Mat();
+                        Mat dst = new Mat();
+                        Utils.bitmapToMat(bitmap, src);
+                        Imgproc.cvtColor(src, dst, Imgproc.COLOR_BGR2GRAY);
+                        Imgproc.Canny(src, dst, 10, 100);
+
+                        Mat h = new Mat();
+                        List<MatOfPoint> list = new ArrayList<>();
+                        Imgproc.findContours(dst, list, h, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+
+                        Mat m = new Mat();
+                        m.create(dst.rows(), dst.cols(), CvType.CV_8UC3);
+                        Random random = new Random();
+                        for (int i = 0; i < list.size(); i++) {
+                            Imgproc.drawContours(m, list, i, new Scalar(random.nextInt(255), random.nextInt(255), random.nextInt(255)), -1);
+                        }
+                        Utils.matToBitmap(m, bitmap);
+                        return bitmap;
+                    }
+                }).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(new Consumer<Bitmap>() {
+                            @Override
+                            public void accept(Bitmap bitmap) throws Exception {
+                                iv.setImageBitmap(bitmap);
+                            }
+                        });
+            }
+        });
+
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CameraActivity.class));
             }
         });
     }
